08422d08ca44daf7f0269a4518602f40ee3c4361
srivastavya02 <srivastavya24@gmail.com>
2025-03-24 19:01:15 +0530

    Setup authentication and user creation with firebase

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -1,24 +1,22 @@
-
 import { getApps, initializeApp, cert } from "firebase-admin/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
 
 export const initFirebaseAdmin = () => {
   const apps = getApps();
 
-  if(!apps.length){
+  if (!apps.length) {
     initializeApp({
-      credential : cert({
+      credential: cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-      })
-    })
+      }),
+    });
   }
 
   return {
     auth: getAuth(),
-    db: getFirestore()
-  }
+    db: getFirestore(),
+  };
 };
